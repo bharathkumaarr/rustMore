4604d5ab577f5ef452051e314835f2375722e4d6
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2024-10-01 02:43:50 +0530

    Create 7-enum-patternmatching-shape.rs

---
+++ 7-enum-patternmatching-shape.rs
@@ -0,0 +1,23 @@
+enum Shape {
+    Rectangle(f64, f64),
+    Circle(f64),
+    Square(f64),
+}
+fn main() {
+    let r = Shape::Rectangle(10.0, 20.0);
+    println!("Area of Rectangle: {}", calculate_area(r));
+
+    let c = Shape::Circle(7.0);
+    println!("Area of Circle: {}", calculate_area(c));
+
+    let s = Shape::Square(4.0);
+    println!("Area of Square: {}", calculate_area(s));
+}
+
+fn calculate_area(shape: Shape) -> f64 {
+    match shape {
+        Shape::Rectangle(a,b) => a*b, 
+        Shape::Circle(r) => 3.1415 * r *r,
+        Shape::Square(side) => side*side,
+    }
+}
